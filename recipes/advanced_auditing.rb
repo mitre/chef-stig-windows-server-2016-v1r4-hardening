bcc90bf2d1d0e54120f5912e535ba2c2b34d3aac
Sturtevant, Alicia A. (ASTURTEVANT) <a.sturtevant4@gmail.com>
2018-09-06 14:08:57 -0400

    updates

--- recipes/advanced_auditing.rb
+++ recipes/advanced_auditing.rb
@@ -1,9 +1,22 @@
-#V-26529, V-26530
-
+#V-73413, V-73415 (credential validation)
+#V-73417 Computer Account Management
+#V-73419 - Other Account Management Events
+#V-73423 - Security Group Management
+#V-73427. V-73429 - User Account Management
+#V-73433 - Process Creation
+#V-73443, V-73445 - Account Lockout
+#V-73447 - Group Memebership
+#V-73449 - logoff
+#V-73451, V-73453 - logon
+#V-73455 - special logon
+#V-73457, V-73459 - removable storage
+#V-73461, V-73463 - audit policy change
 
 powershell_script 'V-1113, V-1114' do
   code <<-EOH
+  Auditpol.exe /set /subcategory:'Group Membership' /success:Enable /Failure:Enable
   Auditpol.exe /set /subcategory:'Credential Validation' /success:Enable /Failure:Enable
+  Auditpol.exe /set /subcategory:'Computer Account Management' /success:Enable /Failure:Enable
   Auditpol.exe /set /subcategory:'Other Account Management Events' /success:Enable /Failure:Disable
   Auditpol.exe /set /subcategory:'Security Group Management' /success:Enable /Failure:Disable
   Auditpol.exe /set /subcategory:'User Account Management' /success:Enable /Failure:Enable
