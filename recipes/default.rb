1bb47c0797cda89f7a5648aed7e1ce9c6b2b3d4b
Rony Xavier <rx294@nyu.edu>
2018-12-21 19:24:37 -0500

    Update default.rb
    Updated to reactivate recipes

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,12 +12,12 @@ powershell_script = <<-EOH
      	wmic computersystem get domainrole | Findstr /v DomainRole
     EOH
 
-#include_recipe 'chef_windows_server_2016_v1r4_stig_hardening::services_and_features'
+include_recipe 'chef_windows_server_2016_v1r4_stig_hardening::services_and_features'
 
 result = powershell_out(powershell_script)
 
 if result.stdout.strip == '4' || result.stdout.strip == '5'
-	#include_recipe 'chef_windows_server_2016_v1r4_stig_hardening::security_policydc'
+	include_recipe 'chef_windows_server_2016_v1r4_stig_hardening::security_policydc'
 	include_recipe 'chef_windows_server_2016_v1r4_stig_hardening::advanced_auditingdc'
 
 end	
@@ -29,6 +29,5 @@ if result.stdout.strip != '4' && result.stdout.strip != '5'
 end
 
 
-#include_recipe 'chef_windows_server_2016_v1r4_stig_hardening::acl'
-#include_recipe 'chef_windows_server_2016_v1r4_stig_hardening::registry_keys'
-
+include_recipe 'chef_windows_server_2016_v1r4_stig_hardening::acl'
+include_recipe 'chef_windows_server_2016_v1r4_stig_hardening::registry_keys'
